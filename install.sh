7a6f0321a2bd824bb8bb0cebf47a920c21b89ba3
Matthew Smailey <them4tthew@gmail.com>
2020-03-19 12:01:40 -0700

    Initial commit

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Verify user is root
+if [ "$EUID" -ne 0 ]
+	then echo "Error: Please run as root"
+	exit 2
+fi
+
+# Detect OS Version
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+
+# Generate dependency install commands by OS
+case "$C" in
+"Arch Linux")
+    COUCHINSTALL = "pacman -S couchdb"
+    ;;
+"2" | "3")
+    echo "..."
+    ;;
+*)
+    echo "..."
+    ;;
+esac
+
+echo "It looks like you are using $OS. Install CouchDB? [Y/N]"
