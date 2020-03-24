569c3d4cd31f5ffb593ab7edeccc528d29823746
Matthew Smailey <them4tthew@gmail.com>
2020-03-23 18:20:50 -0700

    Auto deployment script in progress

--- install.sh
+++ install.sh
@@ -39,19 +39,114 @@ else
 fi
 
 # Generate dependency install commands by OS
-case "$C" in
-"Arch Linux")
-    COUCHINSTALL = "pacman -S couchdb"
+SUPPORTED=0
+
+case "$OS" in
+"Arch Linux" | "Manjaro Linux")
+    COUCHINSTALL="pacman -S couchdb"
+    CURLINSTALL="pacman  -S curl"
+    IPS=`ip address show | grep 'inet ' | awk -F '/' '{print $1}' | awk '{print "    " $2}'`
+    SUPPORTED=1
     ;;
-"Ubuntu")
-    COUCHINSTALL = "apt-get install couchdb"
+"Ubuntu" | "Manjaro Linux")
+    COUCHINSTALL="apt-get install couchdb"
+    CURLNSTALL="apt-get install curl"
+    LISTIPS=`ifconfig | grep 'inet addr' | awk  '{print $2}' | awk -F ':' '{print "    " $2}'`
+    SUPPORTED=1
     ;;
 "FreeBSD")
-    COUCHINSTALL = "pkg install couchdb2"
+    COUCHINSTALL="pkg install couchdb2"
+    CURLINSTALL="pkg install curl"
+    LISTIPS=`ifconfig | grep 'inet ' | awk '{print "    " $2}'`
+    SUPPORTED=1
     ;;
 *)
-    echo ""
+    if [ "$SUPPORTED" != 1 ]; then
+        echo "Error: $OS Not supported. Please install dependencies manually"
+        exit 3
+    fi
     ;;
 esac
 
-echo "It looks like you are using $OS. Install CouchDB? [Y/N]"
+install_package() {
+    printf "\n$1? [Y/N]"
+    read DOINSTALL
+    DOINSTALL=`echo $DOINSTALL | awk '{print toupper($0)}'`
+
+    if [ "$DOINSTALL" == "Y" ]; then
+        `$2`
+    fi
+}
+
+read_with_default() {
+    read RESPONSE
+
+    if [ "$RESPONSE" == "" ]; then
+        echo $1
+    else
+        echo $RESPONSE
+    fi
+}
+
+printf "\n$OS detected.\n\n"
+
+install_package "Install CouchDB" "$COUCHINSTALL"
+install_package "Install cURL" "$CURLINSTALL"
+
+printf "\nThe following IP Addresses have been detected:\n$IPS\n"
+
+echo -n "CouchDB URL [127.0.0.1]:"
+COUCHIP=`read_with_default "127.0.0.1"`
+
+CONFIG=$(cat <<_EOF_
+{
+  "node": {
+    "memoryRecordLimit": 40
+  },
+  "network": {
+    "consensusAlgorithm": "raft",
+    "role": 2,
+    "maxPeers": 10,
+    "discoveryURL": [
+      "http://127.0.0.1"
+    ],
+    "comments": {
+      "consensusAlgorithm": "Currently only 'raft' is available (string)",
+      "role": "Default role of node on network. '0':dynamic, '1':follow, '2':lead (integer)",
+      "maxPeers": "Maximum number of concurrent peer connections (integer)",
+      "discoverURL": "Array of URLs to use when discovering peers"
+    }
+  },
+  "logging": {
+    "enabled": true,
+    "level": 4,
+    "console": true,
+    "file": false,
+    "fileLocation": "/var/log/proliferate",
+    "comments": {
+      "enabled": "Enables/disables all logging. Available values (boolean)",
+      "level": "Log level, higher numbers produce more logs. (integer) Values: '0': Fatal, '1': Error, '2': Warning, '3': Notice, '4': Verbose, '5': Noisy",
+      "console": "Emit logs to console (boolean)'",
+      "file": "Log output file location (string)"
+    }
+  },
+  "couchDB": {
+    "enabled": true,
+    "host": "$COUCHIP",
+    "port": "5984",
+    "protocol": "http",
+    "database": "proliferate",
+    "comments": {
+      "enabled": "Enables/disables CouchDB for block storage",
+      "host": "CouchDB host domain or IP",
+      "port": "CouchDB http port",
+      "protocol": "HTTP protocol 'http' or 'https' (string)"
+    }
+  }
+}
+_EOF_
+)
+
+
+printf "\n$CONFIG\n"
+
